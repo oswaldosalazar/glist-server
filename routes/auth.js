1b6d6b2bf93ab9a4b1f4976c0ae6b24a9eb4f3dc
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-07-20 23:05:59 -0600

    replaced original express code with Herman's

---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// const localAuth = require('../auth/local');
+// const authHelpers = require('../auth/_helpers');
+
+// router.post()
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
+// /* GET home page. */
+// router.get('/', function (req, res, next) {
+//   res.render('index', { title: 'Express and Angular' });
+// });
+
+// module.exports = router;
+
+const express = require('express');
+const router = express.Router();
+
+const indexController = require('../controllers/index');
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express and Angular' });
+  indexController.sum(1, 2, (error, results) => {
+    if (error) return next(error);
+    if (results) {
+      res.json({ sum: results });
+    }
+  });
 });
 
 module.exports = router;
