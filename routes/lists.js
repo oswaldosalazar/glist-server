ce8a74f863f97b10dc68cde5a9675efaa98cbad4
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-09-28 17:36:28 -0600

    Removed commented code

--- routes/lists.js
+++ routes/lists.js
@@ -1,34 +1,24 @@
 const express = require('express');
 const router = express.Router();
-
-// const localAuth = require('../auth/local');
+const authHelpers = require('../auth/_helpers');
 const listsQueries = require('../controllers/lists-queries');
 
-// const indexController = require('../controllers/index');
-
-// router.get('/lists', function (req, res, next) {
-// indexController.sum(1, 2, (error, results) => {
-//   if (error) return next(error);
-//   if (results) {
-//     res.json({ sum: results });
-//   }
-// });
-// });
-
-router.post('/add-list', async (req, res, next) => {
-  try {
-    const dbUser = await listsQueries.addList(req);
-    // const token = localAuth.encodeToken(dbUser.rows[0]);
-    // const firstName = dbUser.rows[0].first_name;
-    res.status(200).json({
-      status: 'success'
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      status: err
-    });
+router.post(
+  '/add-list',
+  authHelpers.ensureAuthenticated,
+  async (req, res, next) => {
+    try {
+      const dbUser = await listsQueries.addList(req);
+      res.status(200).json({
+        status: 'success'
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        status: err
+      });
+    }
   }
-});
+);
 
 module.exports = router;
