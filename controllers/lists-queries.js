74c2be17d3ca375cc27a9214baa595a8e83cd00f
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-11-15 19:42:15 -0700

    Started update list query

--- controllers/lists-queries.js
+++ controllers/lists-queries.js
@@ -1,8 +1,6 @@
-// const localAuth = require('./local');
 const bcrypt = require('bcryptjs');
 const { Pool, Client } = require('pg');
 const connectionString = require('../pg-config');
-// const moment = require('moment');
 
 const client = new Client(connectionString.development.connection);
 
@@ -23,6 +21,23 @@ function addList(req) {
   return client.query(text, values);
 }
 
+// Need to modify text, values to UPDATE query
+function updateList(req) {
+  console.log(req.body);
+  const text =
+    'INSERT INTO auth.lists(user_id, name, picked_items, pending_items, shared_with) VALUES($1, $2, $3, $4, $5) RETURNING *';
+  const values = [
+    req.body.user_id,
+    req.body.name,
+    req.body.picked_items,
+    req.body.pending_items,
+    req.body.shared_with
+  ];
+
+  return client.query(text, values);
+}
+
 module.exports = {
-  addList
+  addList,
+  updateList
 };
